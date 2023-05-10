260cf5f6d6357987af3a156eb2fcdf70a64c7f8d
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-05-11 00:59:12 +0200

    UPDATEEEEEEE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,61 @@
-import { Interfas } from './interfase/Interfase';
+import React, { Component } from 'react';
+import { Statistics } from './Statistics';
+import { FeedbackOptions } from './FeedBackOptions';
+import { Notification } from './Notification';
+import { Section } from './Section';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Interfas></Interfas>
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+  handleLeaveFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleLeaveFeedback}
+          />
+        </Section>
+
+        {totalFeedback === 0 ? (
+          <Notification message="There is no feedback yet" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={positivePercentage}
+            />
+          </Section>
+        )}
+      </>
+    );
+  }
+}
--- src/components/interfase/Notifications.jsx
+++
@@ -1,7 +0,0 @@
-import propTypes from 'prop-types';
-export const Notification = ({ message }) => {
-  return <h2>{message}</h2>;
-};
-Notification.propTypes ={
-    message:propTypes.string
-}
